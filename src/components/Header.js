c562f09236f1e2b9fa1656943b471d9e3ba1774f
Bessy Aguilar <bessy.kga@gmail.com>
2020-01-17 21:34:16 +0100

    create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,35 @@ import { ReactComponent as Logo } from "../images/logo.svg";
 
 class Header extends React.Component {
   render() {
+    const NAV_ITEMS = ["inicio", "personal", "contacto"];
     return (
-      <header>
-        <figure>
-          <Logo title="Bk Aguilar logo" />
+      <header className="header">
+        <figure className="header__figure">
+          <a href="/" className="header__figure__logo">
+            <Logo title="Bk Aguilar logo" />
+          </a>
         </figure>
+        <nav className="header__nav">
+          {NAV_ITEMS.map(item => {
+            return <Item title={item} className="header__nav__item" />;
+          })}
+        </nav>
       </header>
     );
   }
 }
 
+class Item extends React.Component {
+  render() {
+    return (
+      <a
+        href={"#" + this.props.title}
+        title={this.props.title}
+        class={this.props.className}
+      >
+        {this.props.title}
+      </a>
+    );
+  }
+}
 export default Header;
