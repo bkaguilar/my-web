c94558faf850592f3de37a987682aa2672b3fe90
Bessy Aguilar <bessy.kga@gmail.com>
2020-01-28 01:19:18 +0100

    add content

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -7,9 +7,7 @@ class Resume extends React.Component {
       <main className="Resume">
         <header className="Resume__header">
           <h1 className="Resume__header__name">Bessy G. Aguilar</h1>
-          <h2 className="Resume__header__title">
-            Front end en proceso....Zaragoza, España
-          </h2>
+          <h2 className="Resume__header__title">Front end en proceso....</h2>
           <p className="Resume__header__description">
             Engineer valued for driving high-performance accessible web
             experiences. I design quality, user-friendly and scalable products
@@ -17,22 +15,39 @@ class Resume extends React.Component {
           </p>
         </header>
         <article className="Resume__article">
-          <section className="Resume__article__contact">
-            <h3 className="title">Contacto</h3>
-            <ul>
-              <li>hola@bkaguilar.com</li>
-              <li>bkaguilar.com</li>
-              <li>//t.me/bkaguilar</li>
-              <li>Zaragoza, España</li>
-            </ul>
-          </section>
           <section className="Resume__article__education">
             <h3 className="title">Educación</h3>
+            <ul>
+              <li>
+                <span>2007 - 2009</span>
+                <div>
+                  Técnico en computación
+                  <span>INTAE, San Pedro Sula, Honduras.</span>
+                </div>
+              </li>
+              <li>
+                <span>2007 - 2009</span>
+                <div>
+                  Técnico en computación
+                  <span>INTAE, San Pedro Sula, Honduras.</span>
+                </div>
+              </li>
+            </ul>
           </section>
           <section className="Resume__article__experience">
             <h3 className="title">Experiencia Laboral</h3>
+            <ul>
+              <li>
+                <span>2007 - 2009</span>
+                <div>
+                  Baboon
+                  <span>aydiosito.</span>
+                </div>
+              </li>
+            </ul>
           </section>
         </article>
+
         <aside className="Resume__dates">
           <section className="Resume__dates__contact">
             <h3 className="title">Contacto</h3>
@@ -56,6 +71,7 @@ class Resume extends React.Component {
             </ul>
           </section>
         </aside>
+        <section className="loading"></section>
       </main>
     );
   }
