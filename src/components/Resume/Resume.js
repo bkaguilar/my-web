c2e5bbe3cf8953627aadf90fe087167d2ff7dcb2
Bessy Aguilar <bessy.kga@gmail.com>
2020-01-28 12:10:32 +0100

    create components for the sections of the resume page

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,8 +1,52 @@
 import React from "react";
 import "./Resume.scss";
 
+const profile = [
+  {
+    id: 1,
+    sectionName: "Educación",
+    year: "2007 - 2009",
+    title: "Tecnico en computacíon",
+    site: "INTAE, San Pedro Sula, Honduras"
+  },
+  {
+    id: 2,
+    sectionName: "Experiencia Laboral",
+    year: "Marzo 2019 - Septiembre 2019",
+    title: "Diseñadora Web",
+    site: "BaboonLab"
+  }
+];
+
+const data = [
+  {
+    sectionName: "Contacto",
+    items: ["hola@bkaguilar.com", "bkaguilar.com", "t.me/bkaguilar"]
+  },
+  {
+    sectionName: "Habilidades",
+    items: [
+      "Control de versiones GIT",
+      "Docker",
+      "React",
+      "Javascript",
+      "HTML5",
+      "CSS3",
+      "NodeJS",
+      "Suite Adobe(Illustrator, Photoshop, XD, Lightroom)"
+    ]
+  }
+];
+
 class Resume extends React.Component {
   render() {
+    let profileSections = profile.map((item, index) => {
+      return <SumaryProfile key={item.id} item={item} />;
+    });
+
+    let dataSections = data.map((item, index) => {
+      return <TechnicalData key={index} item={item} />;
+    });
     return (
       <main className="Resume">
         <header className="Resume__header">
@@ -14,63 +58,8 @@ class Resume extends React.Component {
             regardless of stack.
           </p>
         </header>
-        <article className="Resume__article">
-          <section className="Resume__article__education">
-            <h3 className="title">Educación</h3>
-            <ul>
-              <li>
-                <span>2007 - 2009</span>
-                <div>
-                  Técnico en computación
-                  <span>INTAE, San Pedro Sula, Honduras.</span>
-                </div>
-              </li>
-              <li>
-                <span>2007 - 2009</span>
-                <div>
-                  Técnico en computación
-                  <span>INTAE, San Pedro Sula, Honduras.</span>
-                </div>
-              </li>
-            </ul>
-          </section>
-          <section className="Resume__article__experience">
-            <h3 className="title">Experiencia Laboral</h3>
-            <ul>
-              <li>
-                <span>2007 - 2009</span>
-                <div>
-                  Baboon
-                  <span>aydiosito.</span>
-                </div>
-              </li>
-            </ul>
-          </section>
-        </article>
-
-        <aside className="Resume__dates">
-          <section className="Resume__dates__contact">
-            <h3 className="title">Contacto</h3>
-            <ul>
-              <li>hola@bkaguilar.com</li>
-              <li>bkaguilar.com</li>
-              <li>//t.me/bkaguilar</li>
-            </ul>
-          </section>
-          <section className="Resume__dates__skills">
-            <h3 className="title">Habilidades</h3>
-            <ul>
-              <li>Git</li>
-              <li>Docker</li>
-              <li>React</li>
-              <li>Javascript</li>
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>NodeJS</li>
-              <li>Suite Adobe(Illustrator, Photoshop, XD, Lightroom)</li>
-            </ul>
-          </section>
-        </aside>
+        <article className="Resume__article">{profileSections}</article>
+        <aside className="Resume__dates">{dataSections}</aside>
         <section className="loading"></section>
       </main>
     );
@@ -78,3 +67,40 @@ class Resume extends React.Component {
 }
 
 export default Resume;
+
+class SumaryProfile extends React.Component {
+  render() {
+    return (
+      <section className="Section">
+        <h3 className="Section__title">{this.props.item.sectionName}</h3>
+        <ul className="Section__list">
+          <li className="Section____list__item">
+            <span className="Section__list__item__year">
+              {this.props.item.year}
+            </span>
+            <div className="Section__list__item__wrapper">
+              {this.props.item.title}
+              <span className="Section__list__item__wrapper__site">
+                {this.props.item.site}
+              </span>
+            </div>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
+
+class TechnicalData extends React.Component {
+  render() {
+    let list = this.props.item.items.map((item, index) => {
+      return <li key={index}>{item}</li>;
+    });
+    return (
+      <section className="TechnicalData">
+        <h3 className="TechnicalData__title">{this.props.item.sectionName}</h3>
+        <ul>{list}</ul>
+      </section>
+    );
+  }
+}
